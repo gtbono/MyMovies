bc81a06572e739d1c7a8ce1eba8fcdf3fb00ce8d
ruby 08 <ruby.08@metralha.cec.ime.usp.br>
2017-01-30 19:34:32 -0200

    Added Layout

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
   post 'movies' => "movies#create"
   get 'movies/:id/edit' => "movies#edit"
   put 'movies/:id' => "movies#update"
-
+	root 'movies#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
